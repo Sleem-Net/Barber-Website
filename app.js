e405ab3df2084f0edd5cfa13b9cc825dd2ae6eca
Oloyede Adeosun <adesleem01@gmail.com>
2022-09-18 19:46:56 +0100

    commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const menuBtn = document.getElementById('menuBtn')
+const sideNav = document.getElementById('sideNav')
+const menu = document.getElementById('menu')
+
+sideNav.style.right = '-250px';
+
+menuBtn.onclick = function () {
+  if (sideNav.style.right == '-250px') {
+    sideNav.style.right = '0';
+    menu.src = './img/close.png';
+  }
+  else {
+    sideNav.style.right = '-250px';
+     menu.src = './img/menu.png';
+  }
+}
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+	speed: 1000,
+	speedAsDuration: true
+});
+
+// Document Object
+
